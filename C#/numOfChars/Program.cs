ab1faceb4b6aa22e7019770f5db1cd0c400e8dac
MADEPADIO <madepadio99@gmail.com>
2023-02-08 03:05:05 +0100

    A program that requests for a string and calculates the length of the string.

---
+++ C#/numOfChars/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numOfChars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Give me a text: ");
+            string text = Console.ReadLine();
+            Console.WriteLine("Your text has " + text.Length + " characters");
+        }
+    }
+}
