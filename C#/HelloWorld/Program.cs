5a25cc657dcb7c65afa4371ee9fa46e7eb7dd2cb
MADEPADIO <madepadio99@gmail.com>
2023-02-08 02:23:08 +0100

    A program that asks user for name, stores the name and says hello to the user.

---
+++ C#/HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is your name? ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name + ", nice to meet you!");
+        }
+    }
+}
