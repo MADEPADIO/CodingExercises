f3e472cc83e01f3ed62417c6ba15b86baf3da40b
MADEPADIO <madepadio99@gmail.com>
2023-02-16 14:45:09 +0100

    Created seperate functions for calculating in sqft and sqm.

--- C#/areaOfRectangle/Program.cs
+++ C#/areaOfRectangle/Program.cs
@@ -8,7 +8,7 @@ namespace areaOfRectangle
 {
     internal class Program
     {
-        const double feetConst = 0.09290304;
+        const double feetTometer = 0.09290304;
         
         //Code (Function) to check if a string is actually a number or not
         public static bool IsAllDigits(string raw)
@@ -48,16 +48,55 @@ namespace areaOfRectangle
             return number;
         }
 
-        static void Main(string[] args)
+        // Calculating the area
+        public static int Area(int length, int width)
+        {
+            return length * width;
+        }
+        
+        // Calculating in Feet
+        public static void Feet()
         {
             int length, width;
             length = IntParser("Input a length: ");
             width = IntParser("Input a width: ");
             Console.WriteLine("You inputted the dimensions of {0} length and {1} width", length, width);
-            var area = length * width;
+            var area = Area(length, width);
             Console.WriteLine("The area is {0} square feet", area);
-            var sqm = area * feetConst;
-            Console.WriteLine("The area is {0} square metres", sqm);
+        }
+
+        // Calculating in Metre
+        public static void Metre()
+        {
+            int length, width;
+            length = IntParser("Input a length: ");
+            width = IntParser("Input a width: ");
+            Console.WriteLine("You inputted the dimensions of {0} length and {1} width", length, width);
+            var area = Area(length, width);
+            Console.WriteLine("The area is {0} square metre", area);
+        }
+        static void Main(string[] args)
+        {
+            string measurement;
+            do
+            {
+                Console.WriteLine("Type your measurement in Foot, 'ft', or Metre, 'm'");
+                measurement = Console.ReadLine();
+                if (measurement == "ft") Feet();
+                else if (measurement == "m") Metre();
+                else Console.WriteLine("Please select either foot or metre");
+            } while ((String.Compare(measurement, "ft") != 0) || (String.Compare(measurement, "m") != 0));
+
+            /*  int length, width;
+              length = IntParser("Input a length: ");
+              width = IntParser("Input a width: ");
+              Console.WriteLine("You inputted the dimensions of {0} length and {1} width", length, width);
+              var area = length * width;
+              Console.WriteLine("The area is {0} square feet", area);
+              var sqm = area * feetTometer;
+              Console.WriteLine("The area is {0} square metres", sqm);*/
+
         }
     }
 }
+// TODO: Implement this program as a GUI program that automatically updates the values when any value changes.
